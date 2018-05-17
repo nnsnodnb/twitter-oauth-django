6d777cf45fa9b4dd5484d100633c22c7618a3f0b
nnsnodnb <ahr63_gej@me.com>
2018-05-17 21:25:06 +0900

    [WIP] Add AuthView in views.py

--- twitter_oauth_ios/urls.py
+++ twitter_oauth_ios/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
+from .views import AuthView
 
 
 app_name = 'twitter_oauth_ios'
 
 
 urlpatterns = [
-    # path('', ),
+    path('', AuthView.as_view(), name='auth_view'),
 ]
--- twitter_oauth_ios/views.py
+++ twitter_oauth_ios/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+from django.http.response import HttpResponse
+from django.views import View
 
-# Create your views here.
+
+class AuthView(View):
+    form_class = None
+
+    def post(self, request, *args, **kwargs):
+        # form = self.form_class(request.POST)
+        return HttpResponse()
