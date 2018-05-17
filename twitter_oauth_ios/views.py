23998d865dc2e209bec813eea9c61100a4c5d555
nnsnodnb <ahr63_gej@me.com>
2018-05-17 21:33:28 +0900

    Update AuthView

--- twitter_oauth_ios/views.py
+++ twitter_oauth_ios/views.py
@@ -1,10 +1,19 @@
 from django.http.response import HttpResponse
 from django.views import View
+from django.views.decorators.csrf import csrf_exempt
+from django.utils.decorators import method_decorator
+
+import json
 
 
 class AuthView(View):
     form_class = None
 
     def post(self, request, *args, **kwargs):
-        # form = self.form_class(request.POST)
+        body = json.loads(request.body)
+        # form = self.form_class(body)
         return HttpResponse()
+
+    @method_decorator(csrf_exempt)
+    def dispatch(self, *args, **kwargs):
+        return super(AuthView, self).dispatch(*args, **kwargs)
