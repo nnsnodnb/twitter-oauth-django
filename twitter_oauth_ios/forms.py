8484ad89da761fcaa2ce55794e758e0906052b3a
nnsnodnb <ahr63_gej@me.com>
2018-05-20 21:23:22 +0900

    Create User models & UserSocialAuth

--- twitter_oauth_ios/forms.py
+++ twitter_oauth_ios/forms.py
@@ -1,5 +1,11 @@
-from django.forms import forms
+from datetime import datetime
+from django import forms
 
 
 class TwitterAuthForm(forms.Form):
-    pass
+    auth_time = forms.IntegerField(min_value=0, initial=int(datetime.now().strftime('%s')), required=True)
+    user_id = forms.IntegerField(min_value=0, required=True)
+    oauth_token = forms.CharField(required=True)
+    oauth_token_secret = forms.CharField(required=True)
+    screen_name = forms.CharField(required=True)
+    display_name = forms.CharField(required=True)
--- twitter_oauth_ios/views.py
+++ twitter_oauth_ios/views.py
@@ -1,18 +1,64 @@
-from django.http.response import HttpResponse
+from datetime import datetime
+from django.contrib.auth import get_user_model
+from django.http.response import JsonResponse
 from django.views import View
 from django.views.decorators.csrf import csrf_exempt
 from django.utils.decorators import method_decorator
+from social_django.models import UserSocialAuth
+from .forms import TwitterAuthForm
 
 import json
 
 
 class AuthView(View):
-    form_class = None
+    form_class = TwitterAuthForm
+    user_model = get_user_model()
 
-    def post(self, request, *args, **kwargs):
+    def put(self, request, *args, **kwargs):
         body = json.loads(request.body)
-        # form = self.form_class(body)
-        return HttpResponse()
+        body['auth_time'] = int(datetime.now().strftime('%s'))
+        form = self.form_class(body)
+        if form.is_valid():
+            social_auth = UserSocialAuth.get_social_auth(provider='twitter', uid=form.data['user_id'])
+
+            if social_auth:
+                user = social_auth.user
+                user.username = form.data['screen_name']
+                user.first_name = form.data['display_name']
+                user.save()
+                social_auth.user = user
+
+                extra_data = social_auth.extra_data
+                extra_data['auth_time'] = form.data['auth_time']
+                extra_data['access_token']['screen_name'] = form.data['screen_name']
+
+                social_auth.extra_data = extra_data
+                social_auth.save()
+
+                return JsonResponse({'result': 'success'})
+
+            user = self.user_model.objects.create_user(
+                username=form.data['screen_name'],
+                first_name=form.data['display_name'],
+                is_active=True)
+            user.save()
+
+            extra_data = {
+                'auth_time': form.data['auth_time'], 'id': form.data['user_id'],
+                'access_token': {
+                    'oauth_token': form.data['oauth_token'],
+                    'oauth_token_secret': form.data['oauth_token_secret'],
+                    'user_id': form.data['user_id'],
+                    'screen_name': form.data['screen_name']
+                }
+            }
+            social_auth = UserSocialAuth(user=user, provider='twitter', uid=form.data['user_id'], extra_data=extra_data)
+            social_auth.save()
+
+            return JsonResponse({'result': 'success'})
+
+        else:
+            return JsonResponse({'result': 'error'}, status=400)
 
     @method_decorator(csrf_exempt)
     def dispatch(self, *args, **kwargs):
